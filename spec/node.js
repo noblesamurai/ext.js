7492dd612710b120ed2a0c59bc71dd456856d899
visionmedia <tj@vision-media.ca>
2010-02-23 14:52:06 -0800

    Removed old node.js testing shit

--- spec/node.js
+++ spec/node.js
@@ -4,19 +4,6 @@ require.paths.unshift('spec', 'spec/lib', 'lib')
 require('jspec')
 require('ext')
 
-quit = process.exit
-print = puts
-
-readFile = function(path) {
-  var result
-  require('posix')
-    .cat(path, "utf8")
-    .addCallback(function(contents){ result = contents })
-    .addErrback(function(){ throw new Error("failed to read file `" + path + "'") })
-    .wait()
-  return result
-}
-
 if (process.ARGV[2])
   JSpec.exec('spec/unit/' + process.ARGV[2] + '.spec.js')  
 else
