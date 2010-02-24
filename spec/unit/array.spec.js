7560c1a931dc11e90313c71326902634382a21c6
Tobias Svensson <tobiassvn@googlemail.com>
2010-02-24 12:47:48 +0000

    Added Array#first and Array#last.

--- spec/unit/array.spec.js
+++ spec/unit/array.spec.js
@@ -16,4 +16,28 @@ describe 'Array'
       [1,2,3,4,5].includes([1,2,3,4,5,6]).should.be_false
     end
   end
+
+  describe '#first'
+    it 'should equal the first element of the array'
+      [1,2,3].first.should.equal 1
+    end
+
+    it 'should be possible to assign the value'
+      a = [1,2,3]
+      a.first = 4
+      a[0].should.eql 4
+    end
+  end
+
+  describe '#last'
+    it 'should equal the last element of the array'
+      [1,2,3].last.should.equal 3
+    end
+
+    it 'should be possible to assign the value'
+      a = [1,2,3]
+      a.last = 4
+      a[2].should.eql 4
+    end
+  end
 end
