04e493e8510fc62e908b79208eaf9ca49915a3d1
Tobias Svensson <tobiassvn@googlemail.com>
2010-02-24 12:44:35 +0000

    Added Array#includes()

--- spec/unit/array.spec.js
+++ spec/unit/array.spec.js
@@ -4,4 +4,16 @@ describe 'Array'
       Array.prototype.each.should.equal Array.prototype.forEach
     end
   end
+
+  describe '#includes()'
+    it 'should indicate whether an array contains an item'
+      ['test'].includes('test').should.be_true
+      ['test'].includes('ok').should.be_false
+    end
+
+    it 'should indicate whether an array contains any of the items in a given array'
+      [1,2,3,4,5].includes([1,2,3,4,5]).should.be_true
+      [1,2,3,4,5].includes([1,2,3,4,5,6]).should.be_false
+    end
+  end
 end
