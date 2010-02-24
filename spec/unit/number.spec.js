eca7135698dc3d3825fd24f02bffe86bb707ca5e
Tobias Svensson <tobiassvn@googlemail.com>
2010-02-24 01:42:27 +0000

    Number specs no longer require exports for Number#currency

--- spec/unit/number.spec.js
+++ spec/unit/number.spec.js
@@ -1,23 +1,21 @@
 
-number = require('ext/number')
-
 describe 'Number'
   describe '#currency'
     describe 'given an integer'
       it 'should return a currency formatted string'
-        number.currency(1).should.eql '1'
-        number.currency(1000).should.eql '1,000'
-        number.currency(100000).should.eql '100,000'
-        number.currency(1000000).should.eql '1,000,000'
+        (1).currency.should.eql '1'
+        (1000).currency.should.eql '1,000'
+        (100000).currency.should.eql '100,000'
+        (1000000).currency.should.eql '1,000,000'
       end
     end
 
     describe 'given a float'
       it 'should return a currency formatted string as a float'
-        number.currency(1.12).should.eql '1.12'
-        number.currency(1000.1).should.eql '1,000.1'
-        number.currency(100000.99).should.eql '100,000.99'
-        number.currency(1000000.55).should.eql '1,000,000.55'
+        (1.12).currency.should.eql '1.12'
+        (1000.1).currency.should.eql '1,000.1'
+        (100000.99).currency.should.eql '100,000.99'
+        (1000000.55).currency.should.eql '1,000,000.55'
       end
     end
   end
