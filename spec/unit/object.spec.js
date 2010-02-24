8af821e06ae14de65062899fa9f9d0b7e8a96a58
Tobias Svensson <tobiassvn@googlemail.com>
2010-02-24 12:18:29 +0000

    Added Object#alias() method.
    While this is purely sugar, it will provide a uniform syntax in
    conjunction with Object#aliasMethodChain() and will make its
    implementation for readable.

--- spec/unit/object.spec.js
+++ spec/unit/object.spec.js
@@ -1,3 +1,4 @@
+
 describe 'Object'
   describe '.prototype enumerables'
     it 'should have none'
@@ -6,4 +7,20 @@ describe 'Object'
       Object.keys(Object.prototype).should.eql []
     end
   end
+
+  describe '#alias()'
+    it 'should alias the given method'
+      obj = { foo: function () {} }
+      obj.alias('fooWithFeature', 'foo')
+      obj.fooWithFeature.should.equal obj.foo
+    end
+
+    it 'should support passing a source object'
+      obj    = {}
+      source = { foo: function () {} }
+      obj.alias('fooWithFeature', 'foo', source)
+      obj.fooWithFeature.should.equal source.foo
+    end
+  end
 end
+
