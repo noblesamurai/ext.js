373d37fe0367af91835913da5e542ab06f20696e
Tobias Svensson <tobiassvn@googlemail.com>
2010-02-25 13:52:11 +0000

    Added Object#values.

--- spec/unit/object.spec.js
+++ spec/unit/object.spec.js
@@ -14,5 +14,11 @@ describe 'Object'
       { foo: 'bar', baz: 'raz' }.keys.should.eql ['foo', 'baz']
     end
   end
+
+  describe '#values'
+    it 'should return an array containing all the values of an object'
+      { a: 1, b: 2, c: 3 }.values.should.eql [1,2,3]
+    end
+  end
 end
 
