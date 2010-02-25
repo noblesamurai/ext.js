964ea4a9689d5decf22e04c227b048a24c68e4d0
visionmedia <tj@vision-media.ca>
2010-02-25 08:02:00 -0800

    Removed Object#extract()

--- spec/unit/object.spec.js
+++ spec/unit/object.spec.js
@@ -20,45 +20,5 @@ describe 'Object'
       { foo: 'bar', baz: 'raz' }.values.should.eql ['bar', 'raz']
     end
   end
-
-  describe '#extract()'
-    it 'should extract options from the given indexed object'
-      var obj = {0: 'a', 1: 'b', 2: {test: 'a'}, length: 3}
-      obj.extract().test.should.eql 'a'
-      obj.length.should.eql 2
-      obj[2].should.be_undefined
-    end
-
-    it 'should return an empty object if no options hash is found'
-      var obj = {0: 'a', 1: 'b', length: 2}
-      obj.extract().should.eql {}
-      obj.length.should.eql 2
-    end
-
-    it 'should return the options hash from the given index'
-      var obj = {0: 'a', 1: {test: 'a'}, 2: 'b', length: 3}
-      obj.extract(1).test.should.eql 'a'
-      obj.length.should.eql 2
-      obj[1].should.eql 'b'
-    end
-
-    it 'should return an empty object if no options hash is found at the given index'
-      var obj = {0: 'a', 1: 'b', 2: 'c', length: 3}
-      obj.extract(1).should.eql {}
-      obj.length.should.eql 3
-      obj[1].should.not.be_undefined
-    end
-
-    it 'should return an empty object if the object is not an indexed object'
-      {foo: 'bar'}.extract().should.eql {}
-      {foo: 'bar'}.extract(42).should.eql {}
-    end
-
-    it 'should support named keys'
-      var obj = {options: {foo: 'bar'}}
-      obj.extract('options').foo.should.eql 'bar'
-      obj.options.should.be_undefined
-    end
-  end
 end
 
