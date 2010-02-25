a78b9f94a8d83d66a9c3d38457661b6a38a72156
visionmedia <tj@vision-media.ca>
2010-02-24 16:32:46 -0800

    MY BAD! forgot files

---
+++ spec/unit/object.spec.js
@@ -0,0 +1,17 @@
+
+describe 'Object'
+  describe '#clone'
+    it 'should return a copy'
+      var a = { foo: 'bar' }
+      var b = a.clone
+      a.should.not.equal b
+      b.should.have_property 'foo', 'bar'
+    end
+  end
+  
+  describe '#keys'
+    it 'should return own property keys'
+      { foo: 'bar', baz: 'raz' }.keys.should.eql ['foo', 'baz']
+    end
+  end
+end
