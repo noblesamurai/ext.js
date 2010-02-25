f0804e120b78087d7ec0afbd0f74ff13cacbf865
Tobias Svensson <tobiassvn@googlemail.com>
2010-02-25 12:31:44 +0000

    Moved String specs to reflect lib dir structure.

--- spec/unit/string/inflections.spec.js
+++ spec/unit/string/inflections.spec.js
@@ -18,4 +18,55 @@ describe 'String inflections'
       }
     end
   end
+
+  describe '#uppercase'
+    it 'should alias toUpperCase()'
+      'foo'.uppercase.should.eql 'FOO'
+    end
+  end
+
+  describe '#lowercase'
+    it 'should alias toLowerCase()'
+      'FOO'.lowercase.should.eql 'foo'
+    end
+  end
+
+  describe '#capitalize()'
+    describe 'given no arguments'
+      it 'should capitalize the first word'
+        'foo bar'.capitalize().should.eql 'Foo bar'
+      end
+    end
+
+    describe 'given true'
+      it 'should capitalize all words'
+        'foo bar baz'.capitalize(true).should.eql 'Foo Bar Baz'
+        'foo bar baz'.capitalize('all').should.eql 'Foo Bar Baz'
+      end
+    end
+  end
+
+  describe '#camelcase'
+    describe 'given several words'
+      it 'should convert them to camel case'
+        'foo bar baz'.camelcase.should.eql 'FooBarBaz'
+      end
+    end
+
+    describe 'given several words with arbitrary characters'
+      it 'should convert them to camel case and disregard the arbitrary characters'
+        'foo-bar-baz'.camelcase.should.eql 'FooBarBaz'
+        'foo_bar_baz'.camelcase.should.eql 'FooBarBaz'
+        'foo-bar_baz'.camelcase.should.eql 'FooBarBaz'
+        'base 64-encode'.camelcase.should.eql 'Base64Encode'
+      end
+    end
+  end
+
+  describe '#digitize'
+    it 'should strip non numeric characters'
+      '$100,000'.digitize.should.eql '100000'
+    end
+  end
+
 end
