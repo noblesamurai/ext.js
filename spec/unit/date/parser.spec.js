1d3ce624504a6e1ba33577806c537788fd4324e7
visionmedia <tj@vision-media.ca>
2010-02-26 09:05:00 -0800

    Refactoring specs

--- spec/unit/date/parser.spec.js
+++ spec/unit/date/parser.spec.js
@@ -4,39 +4,39 @@ parse = require('ext/date').parse
 describe 'date'
   describe 'parse()'
     describe '<day>'
-      it 'should work with "monday"'
-        parse('monday').day.should.eql 1
-      end
-      
-      it 'should work with "tuesday"'
-        parse('tuesday').day.should.eql 2
-      end
-      
-      it 'should work with "wednesday"'
-        parse('wednesday').day.should.eql 3
-      end
-      
-      it 'should work with "thursday"'
+      it 'should work like "next <day>"'
         parse('thursday').day.should.eql 4
-      end
-      
-      it 'should work with "friday"'
-        parse('friday').day.should.eql 5
-      end
-      
-      it 'should work with "saturday"'
-        parse('saturday').day.should.eql 6
-      end
-      
-      it 'should work with "sunday"'
-        parse('sunday').day.should.eql 0
       end
     end
     
     describe 'next <day>'
       describe 'given a <day>'
-        it 'should work'
+        it 'should work with "monday"'
+          parse('next monday').day.should.eql 1
+        end
+
+        it 'should work with "tuesday"'
+          parse('next tuesday').day.should.eql 2
+        end
+
+        it 'should work with "wednesday"'
+          parse('next wednesday').day.should.eql 3
+        end
+
+        it 'should work with "thursday"'
           parse('next thursday').day.should.eql 4
+        end
+
+        it 'should work with "friday"'
+          parse('next friday').day.should.eql 5
+        end
+
+        it 'should work with "saturday"'
+          parse('next saturday').day.should.eql 6
+        end
+
+        it 'should work with "sunday"'
+          parse('next sunday').day.should.eql 0
         end
       end
       
