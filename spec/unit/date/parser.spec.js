a8bfb84a179283c382359b4ecd02c0ed037f33e1
visionmedia <tj@vision-media.ca>
2010-02-26 08:42:17 -0800

    Parsing "<day>"

--- spec/unit/date/parser.spec.js
+++ spec/unit/date/parser.spec.js
@@ -5,9 +5,32 @@ describe 'date'
   describe 'parse()'
     describe '<day>'
       it 'should work with "monday"'
-        var date = parse('monday')
-        
-      end  
+        parse('monday').day.should.eql 1
+      end
+      
+      it 'should work with "tuesday"'
+        parse('tuesday').day.should.eql 2
+      end
+      
+      it 'should work with "wednesday"'
+        parse('wednesday').day.should.eql 3
+      end
+      
+      it 'should work with "thursday"'
+        parse('thursday').day.should.eql 4
+      end
+      
+      it 'should work with "friday"'
+        parse('friday').day.should.eql 5
+      end
+      
+      it 'should work with "saturday"'
+        parse('saturday').day.should.eql 6
+      end
+      
+      it 'should work with "sunday"'
+        parse('sunday').day.should.eql 0
+      end
     end
     
     describe 'next <day>'
