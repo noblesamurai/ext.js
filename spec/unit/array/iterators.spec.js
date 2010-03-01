ce9ec3a1cebd14b52494d1babdca607bbdd6e987
Tobias Svensson <tobiassvn@googlemail.com>
2010-03-01 11:41:46 +0000

    Added aliases for Array#any() and Array#select().

--- spec/unit/array/iterators.spec.js
+++ spec/unit/array/iterators.spec.js
@@ -110,6 +110,23 @@ describe 'Array'
     end
   end
 
+  describe '#select()'
+    it 'should collect values when truthy'
+      var evens = 1..10.select(function(n){ return n % 2 === 0 })
+      evens.should.eql [2,4,6,8,10]
+    end
+
+    it 'should allow optional context'
+      var obj = { foo: function(){}}
+      obj.should.receive('foo', 'twice')
+      ;[1,2].select(function(){ return this.foo() }, obj)
+    end
+
+    it 'should work with shorthand function syntax'
+      1..10.select('a % 2 === 0').should.eql [2,4,6,8,10]
+    end
+  end
+
   describe '#reject()'
     it 'should reject values when truthy'
       var odds = 1..10.reject(function(n){ return n % 2 === 0 })
@@ -167,6 +184,26 @@ describe 'Array'
 
     it 'should work with shorthand function syntax'
       1..5.some('a == 4').should.be_true
+    end
+  end
+
+  describe '#any()'
+    it 'should return true if any evaluate to true'
+      [4,6,4,1,2].any(function(n){ return n > 5 }).should.be_true
+    end
+
+    it 'should return false when none evaluate to true'
+      1..5.any(function(n){ return n > 5 }).should.be_false
+    end
+
+    it 'should allow optional context'
+      var obj = { foo: function(){}}
+      obj.should.receive('foo', 'twice')
+      ;[1,2].any(function(){ return this.foo() }, obj)
+    end
+
+    it 'should work with shorthand function syntax'
+      1..5.any('a == 4').should.be_true
     end
   end
 
