49e20f5f271e90192b1004ca3b803afc7bf7a71a
visionmedia <tj@vision-media.ca>
2010-03-02 18:20:10 -0800

    More benchmarks

--- benchmarks/enumerable.js
+++ benchmarks/enumerable.js
@@ -1,9 +1,19 @@
 
-var n = 50000,
+var n = 400000,
     array = []
 
 while (n--)
   array.push(n)
+
+exports['array for loop'] = function(){
+  for (var i = 0; i < array.length; ++i)
+    ;
+}
+
+exports['array for loop cached'] = function(){
+  for (var i = 0, len = array.length; i < len; ++i)
+    ;
+}
   
 exports['array#each()'] = function(){
   array.each(function(){})
