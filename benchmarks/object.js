076b414e371fde3ce7f04b18da60317e47b31193
visionmedia <tj@vision-media.ca>
2010-04-21 13:11:06 -0700

    Added Object#merge() benchmark

--- benchmarks/object.js
+++ benchmarks/object.js
@@ -21,12 +21,25 @@ exports['Object#keys'] = function(){
 
 var o = {}
 
-exports['regular'] = function(){
+exports['Object.keys()'] = function(){
   for (var i = 0; i < n; ++i) {
     o.foo = 'bar'
     o.foo
     o.bar = 'baz'
     o.bar
     Object.keys(o)
+  }
+}
+
+var a = { foo: 'bar' },
+    b = {},
+    i = 40
+
+while (i--)
+  b[i] = i
+
+exports['Object#merge()'] = function(){
+  for (var i = 0; i < n; ++i) {
+    a.merge(b)
   }
 }
